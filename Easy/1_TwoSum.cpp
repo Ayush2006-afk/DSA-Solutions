df40fc0e219c03af99af099899047358fbb72b1b
Ayush <ayushravidas12@gmail.com>
2026-09-05 19:40:18 +0530

    Solutions

--- Easy/1_TwoSum.cpp
+++ Easy/1_TwoSum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>mp;
+        unordered_map<int,int>mp;  //unordered hashmap
         for(int i=0;i<nums.size();i++){
             int req=target-nums[i];
             if(mp.find(req)!=mp.end()){
